e96528876eebdc41dbb2f363152a227a188c073b
Ayaz Qureshi <ayaz_cont1@olacabs.com>
2020-03-13 21:03:53 +0530

    first commit

---
+++ app/src/main/java/in/insideandroid/navigationgraphsample/MainActivity.kt
@@ -0,0 +1,21 @@
+package `in`.insideandroid.navigationgraphsample
+
+import android.app.Activity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
